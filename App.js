9eaeedd226783c9403b673083fc7bf2b2c819021
tomasnikov <tomasnikov@gmail.com>
2018-03-03 18:50:57 +0000

    Adding map

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu",
 });
 
 type Props = {};
@@ -23,70 +16,110 @@ export default class App extends Component<Props> {
       lastLongitude: null,
       latitude: null,
       longitude: null,
-      error: null
+      error: null,
+      markers: [],
     };
   }
+
+  updatePosition() {
+    const { latitude, longitude, markers } = this.state;
+
+    this.setState({
+      lastLatitude: latitude || position.coords.latitude,
+      lastLongitude: longitude || position.coords.longitude,
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      error: null,
+    });
+  }
+
   componentDidMount() {
+    const { latitude, longitude, markers } = this.state;
+    const options = { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 };
+
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log("current position");
-        this.setState({
-          lastLatitude: this.state.latitude || position.coords.latitude,
-          lastLongitude: this.state.longitude || position.coords.longitude,
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null
-        });
+        this.updatePosition();
       },
       error => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 30000, maximumAge: 1000 }
+      options
     );
-    navigator.geolocation.watchPosition(
+    this.watchId = navigator.geolocation.watchPosition(
       position => {
-        console.log("position changed");
-        this.setState({
-          lastLatitude: this.state.latitude || position.coords.latitude,
-          lastLongitude: this.state.longitude || position.coords.longitude,
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null
-        });
+        console.log("updating position");
+        this.updatePosition();
       },
       error => this.setState({ error: error.message }),
       {
-        enableHighAccuracy: true,
-        timeout: 30000,
-        maximumAge: 1000,
-        distanceFilter: 1
+        ...options,
+        distanceFilter: 1,
       }
     );
   }
+
   componentWillUnmount() {
     navigator.geolocation.clearWatch(this.watchId);
   }
+
+  renderMap() {
+    let { latitude, longitude, markers } = this.state;
+    latitude = latitude || 37.78825;
+    longitude = longitude || -122.4324;
+    console.log(this.state);
+    return (
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0,
+          longitudeDelta: 0.04,
+        }}
+      >
+        {markers.map(marker => (
+          <Marker
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+          />
+        ))}
+      </MapView>
+    );
+  }
+
+  renderText() {
+    const { error, lastLatitude, lastLongitude, latitude, longitude } = this.state;
+
+    return (
+      <Text style={styles.welcome}>
+        {error && (
+          <Text>
+            There was an error{"\n"}
+            {error}
+            {"\n"}
+          </Text>
+        )}
+        Your last position was
+        {"\n"}
+        Latitude: {lastLatitude}
+        {"\n"}
+        Longtitude: {lastLongitude}
+        {"\n"}
+        Your current position is
+        {"\n"}
+        Latitude: {latitude}
+        {"\n"}
+        Longtitude: {longitude}
+      </Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {this.state.error && (
-            <Text>
-              There was an error{"\n"}
-              {this.state.error}
-              {"\n"}
-            </Text>
-          )}
-          Your last position was
-          {"\n"}
-          Latitude: {this.state.lastLatitude}
-          {"\n"}
-          Longtitude: {this.state.lastLongitude}
-          {"\n"}
-          Your current position is
-          {"\n"}
-          Latitude: {this.state.latitude}
-          {"\n"}
-          Longtitude: {this.state.longitude}
-        </Text>
+        {this.renderMap()}
+        {this.renderText()}
       </View>
     );
   }
@@ -94,19 +127,24 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+  },
+  map: {
+    position: "relative",
+    top: 0,
+    height: 400,
+    width: 400,
   },
   welcome: {
     fontSize: 20,
     textAlign: "center",
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: "center",
     color: "#333333",
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
